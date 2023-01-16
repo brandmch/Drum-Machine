9ce4b3fde6cb050499252496870c3a0c2f5fce36
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-16 17:11:35 -0500

    swag

--- src/App.js
+++ src/App.js
@@ -2,48 +2,18 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import soundArr from "./sounds/sounds";
 
-const useAudio = (sound) => {
-  const audio = useState(sound);
-  const [playing, setPlaying] = useState(false);
-
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  }, [playing, audio]);
-
-  useEffect(() => {
-    audio.addEventListener("ended", () => setPlaying(false));
-    return () => {
-      audio.removeEventListener("ended", () => setPlaying(false));
-    };
-  });
-
-  return [playing, toggle];
-};
-
-const SoundButton = (props) => {
-  const [playing, toggle] = useAudio(props.tempAudio);
-
-  return (
-    <div className="button-grid-item" onClick={toggle}>
-      <h2>{playing ? "Pause" : "Play"}</h2>
-    </div>
-  );
-};
-
 function App() {
   const SoundButtons = () => {
     return soundArr.map((curr, ind) => {
       const tempAudio = new Audio(curr);
-      return <SoundButton tempAudio={tempAudio} />;
+
+      return <div className="button-grid-item" id={ind}></div>;
     });
   };
 
   return (
     <div className="App">
       <div className="button-grid-container">
-        <h1>HEAD</h1>
         <SoundButtons />
       </div>
     </div>
