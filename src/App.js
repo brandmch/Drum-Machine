554a00f5b151cf9cbe4df4adc01169e9163d1d26
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-17 10:56:38 -0500

    added event listeners

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import "./App.css";
 import soundArr from "./sounds/sounds";
 
-window.document.onkeydown = (e) => {
-  let { key } = e;
-  switch (key) {
-    case "1":
-      new Audio(soundArr[6]).play();
-      break;
-    case "2":
-      new Audio(soundArr[7]).play();
-      break;
-    case "3":
-      new Audio(soundArr[8]).play();
-      break;
-    case "4":
-      new Audio(soundArr[3]).play();
-      break;
-    case "5":
-      new Audio(soundArr[4]).play();
-      break;
-    case "6":
-      new Audio(soundArr[5]).play();
-      break;
-    case "7":
-      new Audio(soundArr[0]).play();
-      break;
-    case "8":
-      new Audio(soundArr[1]).play();
-      break;
-    case "9":
-      new Audio(soundArr[2]).play();
-      break;
-  }
-};
-
 function App() {
   const soundBoxOrder = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+
+  window.document.onkeydown = (e) => {
+    let { key } = e;
+    switch (key) {
+      case "1":
+        new Audio(soundArr[6]).play();
+        break;
+      case "2":
+        new Audio(soundArr[7]).play();
+        break;
+      case "3":
+        new Audio(soundArr[8]).play();
+        break;
+      case "4":
+        new Audio(soundArr[3]).play();
+        break;
+      case "5":
+        new Audio(soundArr[4]).play();
+        break;
+      case "6":
+        new Audio(soundArr[5]).play();
+        break;
+      case "7":
+        new Audio(soundArr[0]).play();
+        break;
+      case "8":
+        new Audio(soundArr[1]).play();
+        break;
+      case "9":
+        new Audio(soundArr[2]).play();
+        break;
+    }
+  };
+
+  function changeBg(key) {
+    document.getElementById("1").style.backgroundColor = "green";
+  }
+  document.addEventListener("keydown", changeBg);
+
   return (
     <div className="App">
       <div className="controller">
@@ -44,6 +50,7 @@ function App() {
             return (
               <div
                 key={curr}
+                id={ind}
                 className="button-grid-item"
                 onClick={() => {
                   new Audio(curr).play();
