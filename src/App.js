484ee4bf76885aa455b476697dc1b6df989ea13a
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-16 19:16:19 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import "./App.css";
 import soundArr from "./sounds/sounds";
 
-const SoundButton = (props) => {
-  window.document.onkeydown = (e) => {
-    let { key } = e;
-    if (key === `${props.ind + 1}`) {
-      new Audio(props.audio).play();
-    }
-  };
+window.document.onkeydown = (e) => {
+  let { key } = e;
 
-  return (
-    <div
-      className="button-grid-item"
-      onClick={() => {
-        new Audio(props.audio).play();
-      }}
-    ></div>
-  );
+  switch (key) {
+    case "1":
+      new Audio(soundArr[6]).play();
+      break;
+    case "2":
+      new Audio(soundArr[7]).play();
+      break;
+    case "3":
+      new Audio(soundArr[8]).play();
+      break;
+    case "4":
+      new Audio(soundArr[3]).play();
+      break;
+    case "5":
+      new Audio(soundArr[4]).play();
+      break;
+    case "6":
+      new Audio(soundArr[5]).play();
+      break;
+    case "7":
+      new Audio(soundArr[0]).play();
+      break;
+    case "8":
+      new Audio(soundArr[1]).play();
+      break;
+    case "9":
+      new Audio(soundArr[2]).play();
+      break;
+  }
 };
 
 function App() {
-  const SoundButtons = () => {
-    return soundArr.map((curr, ind) => {
-      return <SoundButton audio={curr} ind={ind} key={curr} />;
-    });
-  };
-
   return (
     <div className="App">
       <div className="button-grid-container">
-        <SoundButtons />
+        {soundArr.map((curr) => {
+          return (
+            <div
+              key={curr}
+              className="button-grid-item"
+              onClick={() => {
+                new Audio(curr).play();
+              }}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
